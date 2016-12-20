b8d91b4d3a45f6e7b3b72cf872225d1c7e8057b9
prussian <genunrest@gmail.com>
2016-12-20 17:29:50 -0500

    testing

--- index.js
+++ index.js
@@ -71,35 +71,36 @@ var generateBlob = (tree) => {
 //    }).bundle().pipe(process.stdout)
 }
 
-var tree = {
-    tasks: [
-        { 
-            name: 'test',
-            echo: {
-                x: 'x',
-                y: 'y',
-                z: 'z',
-                arr: ['1','2','3']
-            }
-        },
-        {
-            name: 'test2',
-            serial: [
-                {
-                    name: 'test3',
-                    echo: 'echo2'
-                },
-                {
-                    name: 'test4',
-                    echo: 'echo3'
-                }
-            ]
-        },
-        {
-            name: 'test5',
-            echo: 'echo4'
-        }
-    ]
-}
+module.exports = generateBlob
 
-generateBlob(tree)
+//var tree = {
+//    tasks: [
+//        { 
+//            name: 'test',
+//            echo: {
+//                x: 'x',
+//                y: 'y',
+//                z: 'z',
+//                arr: ['1','2','3']
+//            }
+//        },
+//        {
+//            name: 'test2',
+//            serial: [
+//                {
+//                    name: 'test3',
+//                    echo: 'echo2'
+//                },
+//                {
+//                    name: 'test4',
+//                    echo: 'echo3'
+//                }
+//            ]
+//        },
+//        {
+//            name: 'test5',
+//            echo: 'echo4'
+//        }
+//    ]
+//}
+//generateBlob(tree)
